1c047ede68293d13ad520e429a3a09779f38ef6c
zengyuan <1468288624@qq.com>
2022-11-18 22:01:27 +0800

    提交代码

---
+++ config/defaultSettings.ts
@@ -0,0 +1,22 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-components';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: false,
+  headerHeight: 72,
+  logo: '../echat_logo.png',
+  splitMenus: false,
+  pwa: false,
+};
+
+export default Settings;
